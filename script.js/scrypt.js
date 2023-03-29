2de5534c73ff26eae36186e97251505891c1c58b
Tilfea <laiz.kelsky@gmail.com>
2023-03-29 13:15:09 +0300

    lesson 1-7

---
+++ script.js/scrypt.js
@@ -0,0 +1,111 @@
+// Второе задание
+console.log((a1 = 5 % 3), typeof a1);
+console.log((a2 = 3 % 5), typeof a2);
+console.log((a3 = 5 + "3"), typeof a3);
+console.log((a4 = "5" - 3), typeof a4);
+console.log((a5 = 75 + "кг"), typeof a5);
+console.log((a6 = 785 * 653), typeof a6);
+console.log((a7 = 100 / 25), typeof a7);
+console.log((a8 = 0 * 0), typeof a8);
+console.log((a9 = 0 / 2), typeof a9);
+console.log((a10 = 89 / 0), typeof a10);
+console.log((a11 = 98 + 2), typeof a11);
+console.log((a12 = 5 - 98), typeof a12);
+console.log((a13 = (8 + 56 * 4) / 5), typeof a13);
+console.log((a14 = ((9 - 12) * 7) / (5 + 2)), typeof a14);
+console.log((a15 = +"123"), typeof a15);
+console.log((a16 = 1 || 0), typeof a16);
+console.log((a17 = false || true), typeof a17);
+console.log((a18 = true > 0), typeof a18);
+// Второе задание.Типы данных
+// console.log(typeof a1);
+// console.log(typeof a2);
+// console.log(typeof a3);
+// console.log(typeof a4);
+// console.log(typeof a5);
+// console.log(typeof a6);
+// console.log(typeof a7);
+// console.log(typeof a8);
+// console.log(typeof a9);
+// console.log(typeof a10);
+// console.log(typeof a11);
+// console.log(typeof a12);
+// console.log(typeof a13);
+// console.log(typeof a14);
+// console.log(typeof a15);
+// console.log(typeof a16);
+// console.log(typeof a17);
+// console.log(typeof a18);
+// Третье задание
+let width = 23;
+let height = 10;
+let SPryam = width * height;
+console.log(
+  "Площадь прямоугольника со сторонами",
+  width,
+  "см и",
+  height,
+  "см, равна",
+  SPryam,
+  "см квадратных"
+);
+// Четвертое задание
+VCilindra = 10 * (Math.PI * (a7 / 2) ** 2);
+console.log(
+  "Объем цилиндра высотой",
+  "10см",
+  "и диаметром основания, равным",
+  a7,
+  "см равен",
+  VCilindra,
+  "см кубических"
+);
+// Пятое задание
+let SKruga = Math.PI * 5 ** 2;
+console.log(
+  "Площадь круга с радиусом",
+  "5см",
+  "равен",
+  SKruga,
+  "см квадратных"
+);
+// Шестое задание
+let a = 5;
+let b = 7;
+let h = 10;
+let STrap = ((a + b) / 2) * h;
+console.log(
+  "Площадь трапеции с основаниями",
+  a,
+  "и",
+  b,
+  "см",
+  "и высотой",
+  h,
+  "см равен",
+  STrap,
+  "см квадратных"
+);
+// Седьмое задание
+let s = 2000000;
+let p = 10;
+let years = 5;
+let Pereplata = s * (p / 100) * years;
+console.log(
+  "При размере ипотечного кредита в ",
+  s,
+  "рублей и процентной ставке",
+  p,
+  "%, переплата за",
+  years,
+  "лет составит",
+  Pereplata,
+  "рублей"
+);
+// Возьмое задание
+// a = 8;
+// b = 3;
+// a + 2(x - b)=16;
+// b(x + 15) = a + 6x;
+// x + 2x + ax + bx=23780:
+// console.log(x)
